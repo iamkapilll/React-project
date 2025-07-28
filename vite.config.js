4c9e9ed42f93bd1cc9fc20f27d2eca462710c478
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-28 13:24:30 +0545

    deployed

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'  
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
-
+  base: '/React-project/',  // <-- add this line, replace with your repo name
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'  
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//         tailwindcss(),
+
+//   ],
+// })
